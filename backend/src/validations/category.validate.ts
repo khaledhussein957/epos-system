79f3a6d5593e9a5d3d92b599c0d2785e0fe60ff0
Khaled Hussein <khaledhussein957@gmail.com>
2026-04-07 18:02:25 +0300

    create category route and controller as well the input validations

---
+++ backend/src/validations/category.validate.ts
@@ -0,0 +1,13 @@
+import zod from "zod";
+
+export const createCategorySchema = zod.object({
+  name: zod.string().min(1, "Category name is required"),
+});
+
+export const updateCategorySchema = zod.object({
+  name: zod.string().min(1, "Category name is required").optional(),
+});
+
+export const deleteCategorySchema = zod.object({
+  password: zod.string().min(1, "Password is required"),
+});
